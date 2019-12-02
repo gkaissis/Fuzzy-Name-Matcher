228392c0c64234a83b9161894d8c554deb3f45fe
Georgios Kaissis <48105485+gkaissis@users.noreply.github.com>
2019-12-02 14:08:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Fuzzy Name Matcher",
+    name="fuzzy_name_matcher",
     version="0.1.0",
     author="Georgios Kaissis, Friederike Jungmann",
     author_email="",
