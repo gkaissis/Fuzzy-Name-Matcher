4e1b4bf938bc95741e9ff47077dbf4f007960f3e
Georgios Kaissis <48105485+gkaissis@users.noreply.github.com>
2019-12-02 14:05:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 setup(
     name="Fuzzy Name Matcher",
     version="0.1.0",
     author="Georgios Kaissis, Friederike Jungmann",
     author_email="",
-    packages=find_packages(include=["fuzzy.*"]),
-    scripts="fuzzy.py",
-    url="",
+    packages=find_packages(),
+    url="https://github.com/gkaissis/Fuzzy-Name-Matcher",
     license="LICENSE.MD",
     description="Fuzzy Name Matcher.",
     long_description=open("README.MD").read(),
-    install_requires=open("requirements.txt").read(),
+    install_requires=["fuzzywuzzy==0.17.0", "pandas==0.25.1", "python-Levenshtein==0.12.0", "tqdm==4.39.0"],
     python_requires=">=3.7",
 )
