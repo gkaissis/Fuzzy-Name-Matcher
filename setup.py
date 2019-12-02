a02f1bb5fbfeb4d0e2d178e5cd3461490356cf64
Georgios Kaissis <48105485+gkaissis@users.noreply.github.com>
2019-12-02 14:14:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     license="LICENSE.MD",
     description="Fuzzy Name Matcher.",
     long_description=open("README.MD").read(),
+    scripts="fuzzy.py",
     install_requires=["fuzzywuzzy==0.17.0", "pandas==0.25.1", "python-Levenshtein==0.12.0", "tqdm==4.39.0"],
     python_requires=">=3.7",
 )
